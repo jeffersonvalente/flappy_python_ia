d3ea70d608fb6ab0771f67b28bc0c94a5117cc7d
jeffersonvalente <jeffersonvalente1998@gmail.com>
2022-04-06 10:08:34 -0300

    adicionado a classe brid e as funções init, jump e move

--- flappy.ia.py
+++ flappy.ia.py
@@ -17,3 +17,55 @@ BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
 
 
+#classe do passaro
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0 #posição q o passaro olha
+        self.tick_count = 0 #contatodor de movimento do passarinho
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+        
+    def jump(self):
+        self.vel = -10.5 #faz subir na tela
+        self.tick_count = 0
+        self.height = self.y
+        
+    def move(self):
+        self.tick_count += 1
+        
+        #numero de pixels que se movimenta e criando o movimento das imagens
+        d = self.vel*self.tick_count + 1.5*self.tick_count**2
+        
+        if d >= 16:
+            d = 16
+        if d < 0:
+            d -= 2
+    
+        self.y = self.y + d
+        
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+             self.tilt = self.MAX_ROTATION   
+            
+            else:
+             if self.tilt > - 90:
+                 self.tilt -= self.ROT_VEL
+    
+
+
+
+
+
+#jogo em si
+#while True:
+#    bird.move()
+    
