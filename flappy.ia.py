03d0c0a42478890bb18aa2da20af997a999527ce
jeffersonvalente <jeffersonvalente1998@gmail.com>
2022-04-06 16:30:31 -0300

    criado os canos

--- flappy.ia.py
+++ flappy.ia.py
@@ -135,7 +135,32 @@ class Pipe: #criando os canos
         
         return False
     
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
     
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+        
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        
+        #formula para dar continuidade a tela
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+        
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+    
+    def draw(self,win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+  
+  
 #função para rotacionar a imagem              
 def blitRotateCenter(surf, image, topleft, angle):
     rotated_image = pygame.transform.rotate(image, angle)
